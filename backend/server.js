3f3370bf366bb4cb4fbd1a63f69e678c37aff01f
Mayur <bjhkr29@gmail.com>
2026-05-16 21:05:47 +0530

    fix render server binding

--- backend/server.js
+++ backend/server.js
@@ -44,6 +44,7 @@ app.get('/', (req, res) => {
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
